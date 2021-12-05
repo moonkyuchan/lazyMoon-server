9f875d39abcd8eb7c02b7018208dbd8186f1f710
문규찬 <lazychan90@gmail.com>
2021-12-06 02:29:47 +0900

    node.js 교과서 시작

---
+++ Node.js/Practice/index.js
@@ -0,0 +1,15 @@
+const { odd, even } = require("./var");
+const checkNumber = require("./func");
+
+function checkString(str) {
+  if (str.length % 2) {
+    return odd;
+  }
+  return even;
+}
+
+console.log(checkNumber(10));
+console.log(checkString("kyuchan"));
+
+console.log(__filename);
+console.log(__dirname);
